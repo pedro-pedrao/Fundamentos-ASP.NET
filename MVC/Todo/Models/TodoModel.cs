ec0ab846f913818bf54573d4a840d735af5b307e
Ph-Estudos <ph20066@gmail.com>
2026-02-23 14:56:33 -0300

    feat - implement full CRUD for simple API controller

--- MVC/Todo/Models/TodoModel.cs
+++ MVC/Todo/Models/TodoModel.cs
@@ -8,7 +8,7 @@ namespace Todo.Models
     public class TodoModel
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public bool Done { get; set; }
         public DateTime CreatedAt { get; set; }
     }
--- MVC/Todo/Program.cs
+++ MVC/Todo/Program.cs
@@ -1,6 +1,18 @@
+using Todo.Data;
+
+
+//Criação do builder que vai carregar todos os métodos para dar vida e "infra" para a API
 var builder = WebApplication.CreateBuilder(args);
+
+//Adiciona todas as controllers presentes no sistema
+builder.Services.AddControllers();
+
+//Declara o AppDbContext/Banco como um serviço da aplicacao que pode ser chamado em qualquer parte dela (normalmente controllers)
+builder.Services.AddDbContext<AppDbContext>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Adiciona todas as controllers presentes no sistema
+app.MapControllers();
 
 app.Run();
